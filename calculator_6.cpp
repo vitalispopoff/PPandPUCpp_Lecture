5af0e2b9d1177a5d9751bb343e4b92f4a51ec6a0
vitalispopoff <vitalis.popoff@gmail.com>
2021-04-10 00:27:15 +0200

    7.7

--- calculator_6.cpp
+++ calculator_6.cpp
@@ -1,83 +1,112 @@
 #include "std_lib_facilities.h"
 
-const char number = '8';
-const char quit = 'q';
-const char print = '=';
-const string prompt = "> ";
-const string result = "= ";
+const char
+number{'8'},
+quit{'q'},
+print{'='};
 
-class Token {
+const string
+prompt{"> "},
+result{"= "};
+
+class Token
+{
 public:
+
 	char kind;
 	double value;
+
 	Token(char ch)
-		: kind(ch), value(0) { }
-	Token(char ch, double val)
-		: kind(ch), value(val) {}
+		: kind(ch),value(0) { }
+
+	Token(char ch,double val)
+		: kind(ch),value(val) {}
 };
 
-class Token_stream {
+class Token_stream
+{
 private:
-	bool full{ false };
-	Token buffer;
+	bool
+		full{false};
+	Token
+		buffer;
 public:
 	Token_stream();
-	Token get();
+
 	void putback(Token t);
+	void ignore(char c);
+	Token get();
 };
 
 Token_stream::Token_stream()
-	:full(false), buffer(0) {}
+	:full(false),buffer(0) {}
 
-void Token_stream::putback(Token t) {
-	if (full)
+void Token_stream::putback(Token t)
+{
+	if(full)
 		error("putback() into a full buffer");
+
 	buffer = t;
 	full = true;
 }
 
-Token Token_stream::get() {	// using cin in this implementation already falsifies main>while(cin) to true.
-	if (full) {
+void Token_stream::ignore(char c)				// "c represents the kind of token"
+{
+	if(full && c == buffer.kind)				// "first look in buffer"
+	{
+		full = false;
+		return;
+	}
+
+	full = false;
+	char ch = 0;
+
+	while(cin >> ch)
+		if(ch == c)
+			return;
+}
+
+Token Token_stream::get() 						// using cin in this implementation already falsifies main>while(cin) to true.
+{
+	if(full)
+	{
 		full = false;
 		return buffer;
 	}
+
 	char ch;
 	cin >> ch;
-	switch (ch) {
 
-	case quit: 
-	case print: 
-	//case '!': 
-	case '{': 
-	case '}': 
-	case '(': 
-	case ')':		
-	case '+': 
-	case '-':
-		
-	case '*': 
-	case '/': 
+	switch(ch)
+	{
+	case quit:
+	case print:
+
+	case '{': case '}':
+	case '(': case ')':
+
+	case '+': case '-':
+	case '*': case '/':
 	case '%':
+	{
 		return Token(ch);
-
-	case '.': 
-	case '0': 
-	case '1': 
-	case '2': 
-	case '3': 
-	case '4': 
-	case '5': 
-	case '6': 
-	case '7': 
-	case '8': 
-	case '9': {
-		cin.putback(ch);
-		double val;
-		cin >> val;
-		return Token(number, val);
 	}
 
-	default: error("Bad token");
+	case '.': case '0':
+	case '1': case '2': case '3':
+	case '4': case '5': case '6':
+	case '7': case '8': case '9':
+	{
+		cin.putback(ch);
+
+		double val;
+
+		cin >> val;
+		return Token(number,val);
+	}
+
+	default:
+		error("Bad token");
 	}
 }
 
@@ -85,86 +114,128 @@ Token_stream ts;
 
 double expression();
 
-double primary() {
+double primary()
+{
 	Token
 		t = ts.get();
-	switch (t.kind) {
-	case '{': {
+
+	switch(t.kind)
+	{
+
+	case '{':
+	{
 		double
 			d = expression();
+
 		t = ts.get();
-		if (t.kind != '}')
+
+		if(t.kind != '}')
 			error("'}' expected");
+
 		return d;
 	}
-	case '(': {
+
+	case '(':
+	{
 		double
 			d = expression();
+
 		t = ts.get();
-		if (t.kind != ')')
+
+		if(t.kind != ')')
 			error("')' expected");
+
 		return d;
 	}
+
 	case number:
 		return t.value;
+
 	case'-':
 		return -primary();
+
 	case'+':
 		return primary();
+
 	default:
 		error("primary expected");
 	}
 }
 
-double term() {
+double term()
+{
 	double
 		left = primary();
 	Token
 		t = ts.get();
-	while (true) {
-		switch (t.kind) {
+
+	while(true)
+	{
+		switch(t.kind)
+		{
 		case '*':
+		{
 			left *= primary();
 			t = ts.get();
 			break;
-		case '/': {
+		}
+
+		case '/':
+		{
 			double
 				d = primary();
-			if (d == 0)
+
+			if(d == 0)
 				error("divide by zero");
+
 			left /= d;
 			t = ts.get();
 			break;
 		}
-		case '%': {
-			double d = primary();
-			if (d == 0) error("divide by zero");
-			left = fmod(left, d);
+
+		case '%':
+		{
+			double
+				d = primary();
+
+			if(d == 0)
+				error("divide by zero");
+
+			left = fmod(left,d);
 			t = ts.get();
 			break;
 		}
+
 		default:
+		{
 			ts.putback(t);
 			return left;
+		}
 		}
 	}
 }
 
-double expression() {
+double expression()
+{
 	double
 		left = term();
 	Token
 		t = ts.get();
-	while (true) {
-		switch (t.kind) {
+
+	while(true)
+	{
+		switch(t.kind)
+		{
 		case '+':
 			left += term();
 			t = ts.get();
 			break;
+
 		case '-':
 			left += term();
 			t = ts.get();
 			break;
+
 		default:
 			t.value = left;
 			ts.putback(t);
@@ -173,16 +244,42 @@ double expression() {
 	}
 }
 
-void calculate() {
+void clean_up_mess()
+{
+	ts.ignore(print);
+}
 
-	while (cin) {
-		cout << prompt;
-		Token
-			t = ts.get();
-		while (t.kind == print) t = ts.get();
-		if (t.kind == quit) return;
-		ts.putback(t);
-		cout << result << expression() << endl;
+void calculate()
+{
+	while(cin){
+		try
+		{
+			cout 
+				<< prompt;
+			Token
+				t = ts.get();
+
+			while(t.kind == print)
+				t = ts.get();
+
+			if(t.kind == quit)
+				return;
+
+			ts.putback(t);
+
+			cout
+				<< result
+				<< expression()
+				<< endl;
+		}
+		catch(exception& e)
+		{
+			cerr 
+				<< e.what() 
+				<< endl;
+
+			clean_up_mess();
+		}
 	}
 }
 
@@ -192,13 +289,21 @@ int calculatorMain() {
 		keep_window_open();
 		return 0;
 	}
-	catch (exception& e) {
-		cerr << "error: " << e.what() << '\n';
+	catch(exception& e) 
+	{
+		cerr 
+			<< "error: " 
+			<< e.what() 
+			<< endl;
+
 		keep_window_open();
 		return 1;
 	}
-	catch (...) {
-		cerr << "Oops: unknown exception!\n";
+	catch(...) 
+	{
+		cerr 
+			<< "Oops: unknown exception!\n";
+
 		keep_window_open();
 		return 2;
 	}
