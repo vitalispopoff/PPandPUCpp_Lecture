c22a8decd07f160a9bf05676231f3a08af52a3ed
vitalispopoff <vitalis.popoff@gmail.com>
2021-04-18 15:27:27 +0200

    ch 9.7 excluding code to separate time associated namespace. - working nested namespaces

--- _9_more.cpp
+++ _9_more.cpp
@@ -2,8 +2,7 @@
 #include "_9_more.h"
 
 namespace ch9_lib
-{
-
+{	
 	class X
 	{	
 		int 
@@ -30,171 +29,169 @@ namespace ch9_lib
 		int
 			y = x.f(2);		
 	}
-
-
 	
-	class Date;		// declaration of the class moved to the header file.
-
-	//Date::Date() : y{2001}, m{Month::Jan}, d{1} {}
-
-	Date::Date() :
-		y {defaultDate().getYear()},
-		m {defaultDate().getMonth()},
-		d {defaultDate().getDay()}
-		{ }
-
-	Date::Date(Year i1, Month i2,int i3) : y{i1}, m{i2}, d{i3}
-	{ 
-		if (!isValid())
-		{	
-			cout << "BOINK!";
-			throw Invalid{};
-		}	
-	}
-
-	void Date::setYear	(Year y)	{ this -> y = y; }
-	void Date::setMonth	(Month m)	{ this -> m = m; }
-	void Date::setDay	(int d)		{ this -> d = d; }
-
-	Year	Date::getYear	() const { return y; } 
-	Month	Date::getMonth	() const { return m; }
-	int		Date::getDay	() const { return d; }
-	bool	Date::isValid	() const 
-	{ 
-		return 
-			int(Month::Jan) <= int(m) 
-			&& int(m) <= int(Month::Dec); 
-	}
-
-	void Date::add_day	(int i) 	
-	{ 
-		d += i;
-	}
-
-
-
-	class Year;
-
-	Year::Year(int i) : y{i}
+	namespace Chronou
 	{
-		if( i < min || max <= i)
-			throw Invalid();
-	}
+		class Date;		// declaration of the class moved to the header file.
 
-	void Year::setValue	(int i)	{ y = i; }
+		//Date::Date() : y{2001}, m{Month::Jan}, d{1} {}
+
+		Date::Date() :
+			y {defaultDate().getYear()},
+			m {defaultDate().getMonth()},
+			d {defaultDate().getDay()}
+			{ }
+
+		Date::Date(Year i1, Month i2,int i3) : y{i1}, m{i2}, d{i3}
+		{ 
+			if (!isValid())
+			{	
+				cout << "BOINK!";
+				throw Invalid{};
+			}	
+		}
+
+		void Date::setYear	(Year y)	{ this -> y = y; }
+		void Date::setMonth	(Month m)	{ this -> m = m; }
+		void Date::setDay	(int d)		{ this -> d = d; }
+
+		Year	Date::getYear	() const { return y; } 
+		Month	Date::getMonth	() const { return m; }
+		int		Date::getDay	() const { return d; }
+		bool	Date::isValid	() const 
+		{ 
+			return 
+				int(Month::Jan) <= int(m) 
+				&& int(m) <= int(Month::Dec); 
+		}
+
+		void Date::addDay	(int i) 	
+		{ 
+			d += i;
+		}
+
+
+
+		class Year;
+
+		Year::Year(int i) : y{i}
+		{
+			if( i < min || max <= i)
+				throw Invalid();
+		}
+
+		void Year::setValue	(int i)	{ y = i; }
 	
-	int Year::getValue	()	{ return y; }
+		int Year::getValue	()	{ return y; }
 
 
 
-	// enum Month; // unscoped ENUM redeclaring ? no no no
+		// enum Month; // unscoped ENUM redeclaring ? no no no
 
-	Month int_to_month(int i)
-	{
-		if (i < int(Month::Jan) || int(Month::Dec) < i)
-			error("unavailable month value");
-		return Month(i);
-	}
+		Month int_to_month(int i)
+		{
+			if (i < int(Month::Jan) || int(Month::Dec) < i)
+				error("unavailable month value");
+			return Month(i);
+		}
 
-	Month operator++(Month &m)
-	{
-		m = (m == Month::Dec) ? Month::Jan : Month(int(m) + 1);
-		return m;
-	}
+		Month operator++(Month &m)
+		{
+			m = (m == Month::Dec) ? Month::Jan : Month(int(m) + 1);
+			return m;
+		}
 
-	Month operator--(Month &m)
-	{
-		m = (m == Month::Jan) ? Month::Dec : Month(int(m) - 1);
-		return m;
-	}
+		Month operator--(Month &m)
+		{
+			m = (m == Month::Jan) ? Month::Dec : Month(int(m) - 1);
+			return m;
+		}
 
-	//void ch9_4_6(int x,int y)
-	//{
-	//	try
-	//	{
-	//		Date 
-	//			dxy {2004, x, y};
-	//		cout
-	//			//<< dxy	// operand not implemented.
-	//			<< endl;
-	//		dxy.add_day(2);
-	//	}
-	//	catch (Date::Invalid)
-	//	{
-	//		error("invalid date");
-	//	}
-	//}
+		//void ch9_4_6(int x,int y)
+		//{
+		//	try
+		//	{
+		//		Date 
+		//			dxy {2004, x, y};
+		//		cout
+		//			//<< dxy	// operand not implemented.
+		//			<< endl;
+		//		dxy.add_day(2);
+		//	}
+		//	catch (Date::Invalid)
+		//	{
+		//		error("invalid date");
+		//	}
+		//}
 
-	/*	static defaultDate returns static Date. is it unique to each call ?*/
-	void example_7()
-	{
-		Date
-			d1{},
-			d2{};
+		/*	static defaultDate returns static Date. is it unique to each call ?*/
+		void example_7()
+		{
+			Date
+				d1{},
+				d2{};
 
-		d1.setYear(2021);
-		d1.setMonth(Month::Apr);
+			d1.setYear(2021);
+			d1.setMonth(Month::Apr);
 
-		int
-			i1 = d1.getYear().getValue(),
-			i2 = d2.getYear().getValue();
-		bool
-			b = i1 == i2;
+			int
+				i1 = d1.getYear().getValue(),
+				i2 = d2.getYear().getValue();
+			bool
+				b = i1 == i2;
 
-		cout << b;
-	}
+			cout << b;
+		}
 
-	/*declaring vector<Date> because default constructor*/
-	void example_8()
-	{
-		/*use of () instead {} in the vector definition is by convention rather*/
-		vector<Date>
-			birthdays(10);
-		cout 
-			<< "\n\tsize of the vector: " 
-			<< birthdays.size();
-		for(Date d : birthdays)
+		/*declaring vector<Date> because default constructor*/
+		void example_8()
+		{
+			/*use of () instead {} in the vector definition is by convention rather*/
+			vector<Date>
+				birthdays(10);
 			cout 
-				<< "\n\tyear: " << d.getYear().getValue()
-				<< "\tmonth: " << int(d.getMonth())
-				<< "\tday: " << d.getDay();
-		cout 
-			<< endl;
+				<< "\n\tsize of the vector: " 
+				<< birthdays.size();
+			for(Date d : birthdays)
+				cout 
+					<< "\n\tyear: " << d.getYear().getValue()
+					<< "\tmonth: " << int(d.getMonth())
+					<< "\tday: " << d.getDay();
+			cout 
+				<< endl;
+		}
+
+		/* 'startOfTerm' refuses to pass its member to 'b' directly - 'const' prevents	
+		it has no problem with passing itself tho, so it's rather a 'const' vs 'getDay()' */
+		void example_09(Date &d, const Date &startOfTerm)
+		{
+			int a	{d.getDay()};
+			Date tmp{startOfTerm};
+			int b	{tmp.getDay()};
+		}
+
+		// making 'tmp' const prevents it from passing its member to 'b'
+		//void example_09(Date &d,Date &startOfTerm)
+		//{
+		//	int a{d.getDay()};
+		//	const Date tmp{startOfTerm};
+		//	int b{tmp.getDay()};
+		//}
+		void example_10(Date &startOfTerm)
+		{
+			int a {startOfTerm.getDay()};
+		}
 	}
-
-	/* 'startOfTerm' refuses to pass its member to 'b' directly - 'const' prevents	
-	it has no problem with passing itself tho, so it's rather a 'const' vs 'getDay()' */
-	void example_09(Date &d, const Date &startOfTerm)
-	{
-		int a	{d.getDay()};
-		Date tmp{startOfTerm};
-		int b	{tmp.getDay()};
-	}
-
-	// making 'tmp' const prevents it from passing its member to 'b'
-	//void example_09(Date &d,Date &startOfTerm)
-	//{
-	//	int a{d.getDay()};
-	//	const Date tmp{startOfTerm};
-	//	int b{tmp.getDay()};
-	//}
-	void example_10(Date &startOfTerm)
-	{
-		int a {startOfTerm.getDay()};
-	}
-
-
 }
-
-
-
 
 void ch09Main()
 {
 	using namespace ch9_lib;
 	
+
 	//ch9_4();
 	//example7();
 	//example_8();
+
 
 }
--- _9_more.h
+++ _9_more.h
@@ -12,84 +12,84 @@
 
 namespace ch9_lib
 {
-
-	class Year
-	{	
-		public:
-			class Invalid{ };
-			
-			Year (int);
-			
-			void setValue	(int);
-			int	getValue	();
-
-		private:
-			static const int 
-				min{1800},
-				max{2200};
-			int 
-				y;
-	};
-
-	enum class Month
+	namespace Chronou
 	{
-		//Jan,	// by default starts with 0 obviously
-		Jan = 1, // but a different value may be assigned;
-		Feb,
-		Mar,
-		//Mar = 5, to every enum member
-		Apr,
-		May,
-		Jun,
-		Jul,
-		Aug,
-		Sep,
-		Oct,
-		Nov,
-		Dec
-	};
-
-	Month int_to_month(int);
-	Month operator++(Month&);
-	Month operator--(Month&);
-
-	class  Date
-	{
-		public:
-			class Invalid { };		// class used as exception
+		class Year
+		{	
+			public:
+				class Invalid{ };
 			
-			/*Default constructor secures declarations like for vectors*/
-			Date	();
+				Year (int);
+			
+				void setValue	(int);
+				int	getValue	();
 
-			Date	(Year, Month, int);
+			private:
+				static const int 
+					min{1800},
+					max{2200};
+				int 
+					y;
+		};
 
-			void setYear	(Year);
-			void setMonth	(Month);
-			void setDay		(int);
+		enum class Month
+		{
+			//Jan,	// by default starts with 0 obviously
+			Jan = 1, // but a different value may be assigned;
+			Feb,
+			Mar,
+			//Mar = 5, to every enum member
+			Apr,
+			May,
+			Jun,
+			Jul,
+			Aug,
+			Sep,
+			Oct,
+			Nov,
+			Dec
+		};
 
-			Year	getYear	()	const;	// 'const' declares a non invasive function
-			Month	getMonth()	const;
-			int		getDay	()	const;
-			bool	isValid	()	const;
+		Month int_to_month(int);
+		Month operator++(Month&);
+		Month operator--(Month&);
 
-			void add_day	(int);
+		class  Date
+		{
+			public:
+				class Invalid { };		// class used as exception
+			
+				/*Default constructor secures declarations like for vectors*/
+				Date	();
 
-			/*This one apparently has to be implemented inside the class declaration
-				for whatever reason the function doesn't like returning 'const'
-			*/
-			static Date& defaultDate()
-			{
-				static Date
-					dd{2001, Month::Jan, 1};
-				return dd;
-			};
+				Date	(Year, Month, int);
 
-		private:
-			Year	y;
-			Month	m;
-			int		d;
+				void setYear	(Year);
+				void setMonth	(Month);
+				void setDay		(int);
 
-	};
+				Year	getYear	()	const;	// 'const' declares a non invasive function
+				Month	getMonth()	const;
+				int		getDay	()	const;
 
+				void addDay	(int);
 
+				/*This one apparently has to be implemented inside the class declaration
+					for whatever reason the function doesn't like returning 'const'
+				*/
+				static Date& defaultDate()
+				{
+					static Date
+						dd{2001, Month::Jan, 1};
+					return dd;
+				};
+
+			private:
+				Year	y;
+				Month	m;
+				int		d;
+
+				bool	isValid	()	const;
+		};
+	}
 }
